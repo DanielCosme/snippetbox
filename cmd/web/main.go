e91b3921fda0b15bc06bd9310b35b5d0a4864478
Daniel Cosme <cosmedaniel8@gmail.com>
2021-05-01 17:20:59 -0400

    Add session management and Implement flash messages

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -7,15 +7,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/danielcosme/snippetbox/pkg/models/mysql"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
 type application struct {
 	errorLog      *log.Logger
 	infoLog       *log.Logger
 	snippets      *mysql.SnippetModel
+	session       *sessions.Session
 	templateCache map[string]*template.Template
 }
 
@@ -27,6 +30,10 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
 	// SQL Data Source Name (dsn)
 	dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "MySQL data source name")
+	// Define a new command-line flag for the session secret (a random key which
+	// will be used to encrypt and authenticate session cookies). It should be 32
+	// bytes long.
+	secret := flag.String("secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret key")
 	flag.Parse()
 
 	// Use log.New() to create a logger for writing information messages. This takes
@@ -53,11 +60,18 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	// Use the sessions.New() function to initialize a new session manager,
+	// passing in the secret key as the parameter. Then we configure it so
+	// sessions always expires after 12 hours.
+	session := sessions.New([]byte(*secret))
+	session.Lifetime = 12 * time.Hour
+
 	// Initialize a new instance of application containing the dependencies.
 	app := &application{
 		errorLog:      errorLog,
 		infoLog:       infoLog,
 		snippets:      &mysql.SnippetModel{DB: db},
+		session:       session,
 		templateCache: templateCache,
 	}
 
--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -12,11 +12,14 @@ func (app *application) routes() http.Handler {
 	r.Use(app.logRequest)
 	r.Use(secureHeaders)
 
-	r.Get("/", app.home)
+	r.Group(func(r chi.Router) {
+		r.Use(app.session.Enable)
 
-	r.Get("/snippet/create", app.createSnippetForm)
-	r.Post("/snippet/create", app.createSnippet)
-	r.Get("/snippet/{id}", app.showSnippet)
+		r.Get("/", app.home)
+		r.Get("/snippet/create", app.createSnippetForm)
+		r.Post("/snippet/create", app.createSnippet)
+		r.Get("/snippet/{id}", app.showSnippet)
+	})
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	r.Handle("/static/*", http.StripPrefix("/static", fileServer))
